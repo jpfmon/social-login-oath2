27242bda517f0fb1734ed4d449ac5b3843c5330c
Juan Fdez Montojo <jpfmontojo@gmail.com>
2024-03-27 12:43:04 +0200

    First Commit

---
+++ src/main/java/com/montojo/socialloginoath2/SocialLoginOath2Application.java
@@ -0,0 +1,13 @@
+package com.montojo.socialloginoath2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginOath2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginOath2Application.class, args);
+    }
+
+}
